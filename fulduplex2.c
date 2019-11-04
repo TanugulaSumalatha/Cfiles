290a4e888cd616e2e086f542450a9e9792cdf0e0
TanugulaSumalatha <56529570+TanugulaSumalatha@users.noreply.github.com>
2019-11-04 11:46:28 +0530

    Add files via upload
    ful duplex using threads and fifos

---
+++ fulduplex2.c
@@ -0,0 +1,53 @@
+ /***full duplex communication b/n 2 processes using namedpipe(fifo) with the help of threads***/
+	#include<stdio.h>
+	#include<pthread.h>
+	#include<fcntl.h>
+	#include<string.h>
+        #include<unistd.h>
+        #include <sys/types.h>
+        #include <sys/stat.h>
+    void * thread1(void *p)
+      {
+          int fd;
+          char buf[128];
+          fd=open("pipe1",O_RDONLY);
+           if(fd<0)
+             {
+                 perror("open");
+		 return;
+             }
+	   while(1)
+	     {	
+                read(fd,buf,sizeof(buf));
+                printf("received:%s\n",buf);
+
+	     }
+      }
+    void * thread2(void *p)
+      {
+          int fd;
+          char buf[128];
+          fd=open("pipe2",O_WRONLY);
+           if(fd<0)
+             {
+                 perror("open");
+                 return;
+             }
+           while(1)
+             {
+               // scanf("%s",buf);
+                   gets(buf);
+                write(fd,buf,strlen(buf)+1);
+             }
+       }
+    void main()
+     {
+       pthread_t t1,t2;
+       mkfifo("pipe1",0664);  //fifo creates,named pipe
+       mkfifo("pipe2",0664);
+       pthread_create(&t1,0,thread1,0);
+       pthread_create(&t2,0,thread2,0);
+       while(1);
+     }
+
+
